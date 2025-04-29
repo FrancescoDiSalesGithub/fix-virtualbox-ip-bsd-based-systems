a6f83f3194ea8ef191ea7069e12b9d4feeb741f5
Francesco Di Sales <f.disales@hotmail.com>
2025-04-29 21:24:23 +0200

    Create fix-ip.sh

---
+++ fix-ip.sh
@@ -0,0 +1,15 @@
+!#/bin/bash
+
+interface=$1
+ip=$2
+ip_broadcast=$3
+
+user=$(whoami)
+
+if [ "$user" == "root" ]
+then
+  ifconfig $interface ip $ip
+  ifconfig $interface broadcast $ip_broadcast
+else
+  echo "run this script as root"
+fi
